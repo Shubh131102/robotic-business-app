e525aaae289ac2f903379c59ed496f381ee2d8ab
Shubh131102 <sjang041@ucr.edu>
2025-04-27 18:30:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,20 @@ uploaded_file = st.file_uploader("Upload a map image", type=["png", "jpg", "jpeg
 
 if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    image = cv2.imdecode(file_bytes, 0)
+    image = cv2.imdecode(file_bytes, 0)  # Grayscale
 
     st.image(image, caption="Uploaded Map", use_container_width=True)
 
+    # Use session state to store points
+    if "start_point" not in st.session_state:
+        st.session_state.start_point = None
+    if "goal_point" not in st.session_state:
+        st.session_state.goal_point = None
+
+    # Ask user what they want to select
+    select_mode = st.radio("Select Mode", ["Start Point", "Goal Point"])
+
+    # Capture click
+    clicked = st.image(image, caption="Click to select points", use_container_width=True)
+
+    st.write("**Note:** Click functionality will be added soon in Day 2 Part 2.")  # We'll do clicks next
